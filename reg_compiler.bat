e56dddc0060f1ddd042da77c739aa0436b126827
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2024-06-05 18:12:57 +0300

    Update reg_compiler.bat

--- reg_compiler.bat
+++ reg_compiler.bat
@@ -13,6 +13,6 @@ echo.[HKEY_CLASSES_ROOT\Webby\shell\open]>>reg_installer.reg
 echo.@="&Play this Unreal level">>reg_installer.reg
 echo.>>reg_installer.reg
 echo.[HKEY_CLASSES_ROOT\Webby\shell\open\command]>>reg_installer.reg
-echo.@="%calling_path%\webbetter.exe \"%%1\"">>reg_installer.reg
+echo.@="%calling_path%\\webbetter.exe \"%%1\"">>reg_installer.reg
 echo.>>reg_installer.reg
 reg_installer
